e1dbdd40d9582c3bb746edb55fb91e4f4c172421
Nazim Uddin <nazimdugeo@gmail.com>
2026-06-26 22:40:47 +0600

    Disable test ads for release

--- app/src/main/java/com/ipdial/IPDialApplication.kt
+++ app/src/main/java/com/ipdial/IPDialApplication.kt
@@ -12,7 +12,7 @@ class IPDialApplication : Application() {
         // Initialize Start.io SDK
         StartAppSDK.init(this, "205857982", true)
         // Enable test ads to verify integration
-        StartAppSDK.setTestAdsEnabled(true)
+        StartAppSDK.setTestAdsEnabled(false)
         // Disable splash screen ads if desired
         StartAppAd.disableSplash()
 
